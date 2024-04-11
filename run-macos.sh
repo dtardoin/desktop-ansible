58ae1fa3fc5fcacd038988795d889136fd638ce9
David Ardoin <david.ardoin@dbtlabs.com>
2024-04-11 10:20:42 -0400

    Updates Apr 24

--- run-macos.sh
+++ run-macos.sh
@@ -2,5 +2,7 @@
 # Install ansible (and optionally brew)
 
 which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install ansible 
-ansible-playbook --ask-sudo-pass install.yml
+echo "$PATH:/opt/homebrew/bin/" >> ~/.zshrc 
+source ~/.zshrc
+/opt/homebrew/bin/brew install ansible 
+/opt/homebrew/bin/ansible-playbook --ask-become-pass --skip-tags ubuntu install.yml
