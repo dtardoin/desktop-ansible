054e7a48dd36f0d9326bf68488f0b02a0dd8af53
David Ardoin <david@tothecloud.io>
2025-05-10 19:32:37 -0400

    syntax updates

--- run-ubuntu.sh
+++ run-ubuntu.sh
@@ -3,6 +3,6 @@
 
 sudo apt-get update && sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible 
-sudo apt-get update && sudo apt-get install -y ansible python
+sudo apt-get update && sudo apt-get install -y ansible ansible-core python3
 
-ansible-playbook --ask-sudo-pass install.yml
+ansible-playbook --ask-become-pass install.yml
